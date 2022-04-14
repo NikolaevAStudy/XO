f55ced0d0952ec46a99705eff9af2559b4cc19ec
Alexandr Nikolaev <aleksandrnikolaevwork@gmail.com>
2022-04-14 20:03:37 +0300

    Create StartViewController.swift

---
+++ xo/StartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StartViewController.swift
+//  xo
+//
+//  Created by Александр Николаев on 15.03.2022.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+    }
+  
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
