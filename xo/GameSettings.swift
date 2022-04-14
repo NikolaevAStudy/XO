7f2b4642fea07128fa2083abd2b4287e74f77bde
Alexandr Nikolaev <aleksandrnikolaevwork@gmail.com>
2022-04-14 20:03:13 +0300

    Create GameSettings.swift

---
+++ xo/GameSettings.swift
@@ -0,0 +1,15 @@
+//
+//  GameSettings.swift
+//  xo
+//
+//  Created by Александр Николаев on 10.03.2022.
+//
+
+import Foundation
+
+class gameSettings{
+    var players:Int
+    init (players:Int){
+        self.players=players
+    }
+}
