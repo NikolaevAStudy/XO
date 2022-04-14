ea7189f5150ccfdbfaee67326ecc3c6fb66c69bc
Alexandr Nikolaev <aleksandrnikolaevwork@gmail.com>
2022-04-14 20:03:17 +0300

    Create GameUIButton.swift

---
+++ xo/GameUIButton.swift
@@ -0,0 +1,30 @@
+//
+//  GameUIButton.swift
+//  xo
+//
+//  Created by Александр Николаев on 28.02.2022.
+//
+
+import UIKit
+
+class GameUIButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    override func draw(_ rect: CGRect) {
+        self.layer.cornerRadius = 5    /// радиус закругления закругление
+        self.layer.borderWidth = 3.0   // толщина обводки
+        self.layer.borderColor = (UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)).cgColor // цвет обводки
+        self.clipsToBounds = true  // не забудь это, а то не закруглиться
+        self.tintColor = .black
+        self.setTitleColor(.black, for: .normal)
+        self.titleLabel?.font = UIFont.boldSystemFont(ofSize: 75)
+        //self.setTitle("X", for: .normal)
+    }
+        
+}
