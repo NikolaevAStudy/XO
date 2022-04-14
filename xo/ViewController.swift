d5eca3a38f7a91cae74eb7b4828d50870c23ff67
Alexandr Nikolaev <aleksandrnikolaevwork@gmail.com>
2022-04-14 20:03:41 +0300

    Create ViewController.swift

---
+++ xo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xo
+//
+//  Created by Александр Николаев on 26.02.2022.
+//
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var gameButton: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
